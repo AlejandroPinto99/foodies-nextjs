211c0f02d314e1fbcd3a9bc3cfabe92720a78f93
AlejandroPinto99 <00156628@uca.edu.sv>
2021-09-24 11:34:48 -0600

    Add: links to footer

--- components/Footer/Links.js
+++ components/Footer/Links.js
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 const Links = () => {
     return(
         <div className="flex justify-center pb-10 mt-5 mb-10 md:mb-5 md:mt-2 ">
             <ul className="md:w-8/12 md:flex md:justify-between md:flex-wrap md:mx-10">
-                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5"><a href="/#">Conoce nuestras sucursales</a></li>
-                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5" ><a href="/#">Acerca de </a></li>
-                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5" ><a href="/#">¿Qué hablan de nosotros?</a></li>
-                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5"><a href="/#">Contáctanos</a></li>
+                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5"><Link href="/#restaurantes">Conoce nuestras sucursales</Link></li>
+                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5"><Link href="/acerca_de">Acerca de </Link></li>
+                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5"><Link href="/#">¿Qué hablan de nosotros?</Link></li>
+                <li className="text-center my-3 text-gray-400 text-sm font-roboto md:text-lg md:mx-5"><Link  href="/contactanos">Contáctanos</Link></li>
             </ul>
         </div>
     )
